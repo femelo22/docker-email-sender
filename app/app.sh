aca7103e3939f274c858c02243105e8ca76e50ca
Luiz Fernando M Gonçalves <56324956+femelo22@users.noreply.github.com>
2022-04-26 08:22:45 -0300

    Registrando dados no postgres

--- app/app.sh
+++ app/app.sh
@@ -1,5 +1,5 @@
 #instalar um micro-framework web e chamar o código em python para subir o serviço
 
 #!/bin/sh
-pip install bottle==0.12.13
+pip install bottle==0.12.13 psycopg2==2.7.1
 python -u sender.py
